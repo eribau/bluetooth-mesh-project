727f863a07710be32cc597ed8a87c968e69a0075
Mandar Joshi <mandar1997@gmail.com>
2018-04-12 09:52:23 +0200

    Final commit

--- rfcomm-client.c
+++ rfcomm-client.c
@@ -1,3 +1,5 @@
+//Code from https://people.csail.mit.edu/albert/bluez-intro/x502.html - RFCOMM sockets
+
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
@@ -10,11 +12,11 @@ int main(int argc, char **argv)
 {
     struct sockaddr_rc addr = { 0 };
     char buf[1024] = { 0 };
-    int s, status, bytes_read;
-    char dest[18] = "B8:27:EB:9B:D4:87";
+    int connecting_socket, connection_status, bytes_read;
+    char dest[18] = "B8:27:EB:9B:D4:87";                      
 
     // allocate a socket
-    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+    connecting_socket = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
     // set the connection parameters (who to connect to)
     addr.rc_family = AF_BLUETOOTH;
@@ -22,20 +24,22 @@ int main(int argc, char **argv)
     str2ba( dest, &addr.rc_bdaddr );
 
     // connect to server
-    status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
+    connection_status = connect(connecting_socket, (struct sockaddr *)&addr, sizeof(addr));
 
     // send a message
-    if( status == 0 ) {
-        status = write(s, "herro!", 6);
+    if( connection_status == 0 ) {
+        connection_status = write(connecting_socket, "herro!", 6);
     }
     
-    /*bytes_read = read(s, buf, sizeof(buf));
+    // read a message
+    /*bytes_read = read(connecting_socket, buf, sizeof(buf));
 		if( bytes_read > 0 ) {
 			printf("received [%s]\n", buf);
 		}*/
 
-    if( status < 0 ) perror("uh oh");
+    if( connection_status < 0 ) perror("uh oh");
 
-    close(s);
+    close(connecting_socket);
     return 0;
 }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
--- rfcomm-server.c
+++ rfcomm-server.c
@@ -1,3 +1,5 @@
+//Code from https://people.csail.mit.edu/albert/bluez-intro/x502.html - RFCOMM sockets
+
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
@@ -6,37 +8,38 @@
 
 int main(int argc, char **argv)
 {
-    struct sockaddr_rc loc_addr = { 0 }, rem_addr = { 0 };
+    struct sockaddr_rc loc_addr = { 0 }, cli_addr = { 0 };
     char buf[1024] = { 0 };
-    int s, client, bytes_read;
-    socklen_t opt = sizeof(rem_addr);
+    int connection_socket, client, bytes_read;
+    socklen_t len = sizeof(cli_addr);
 
     // allocate socket
-    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+    connection_socket = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
     // bind socket to port 1 of the first available 
     // local bluetooth adapter
     loc_addr.rc_family = AF_BLUETOOTH;
     loc_addr.rc_bdaddr = *BDADDR_ANY;
     loc_addr.rc_channel = (uint8_t) 1;
-    bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
+    bind(connection_socket, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
 
     // put socket into listening mode
-    listen(s, 1);
+    listen(connection_socket, 1);
 
 	while(1){
 					
 		// accept one connection
-		client = accept(s, (struct sockaddr *)&rem_addr, &opt);
+		client = accept(connection_socket, (struct sockaddr *)&cli_addr, &len);
 
-		ba2str( &rem_addr.rc_bdaddr, buf );
-		fprintf(stderr, "accepted connection from %s\n", buf);
+		ba2str( &cli_addr.rc_bdaddr, buf );
+		fprintf(stderr, "accepted connection from %connection_socket\n", buf);
 		memset(buf, 0, sizeof(buf));
 
 		// read data from the client
 		bytes_read = read(client, buf, sizeof(buf));
 		if( bytes_read > 0 ) {
-			printf("received [%s]\n", buf);
+			printf("received [%connection_socket]\n", buf);
+			//Write data to the client
 			write(client, "hi", 2);
 		}
 		
@@ -45,6 +48,6 @@ int main(int argc, char **argv)
 
     // close connection
     
-    close(s);
+    close(connection_socket);
     return 0;
 }
