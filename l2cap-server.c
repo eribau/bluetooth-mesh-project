b7307de2e457e15531bcadb98536c829f4d1bc85
Mandar Joshi <mandar1997@gmail.com>
2018-04-12 16:27:38 +0200

    Modified server

--- l2cap-server.c
+++ l2cap-server.c
@@ -28,11 +28,12 @@ int main(int argc, char **argv)
     loc_addr.l2_bdaddr_type = BDADDR_LE_PUBLIC;
     loc_addr.l2_psm = htobs(0x1001);*/
     
-    hci_device_id = hci_get_route(NULL);
-    hci_socket = hci_open_dev(hci_device_id);
-    loc_addr.l2_family = AF_BLUETOOTH;
+    //hci_device_id = hci_get_route(NULL);
+    //hci_socket = hci_open_dev(hci_device_id);
+    loc_addr.l2_family = AF_BLUETOOTH; 
     loc_addr.l2_bdaddr = *BDADDR_ANY;
-    loc_addr.l2_cid = htobs(ATT_CID);
+    loc_addr.l2_cid = htobs(ATT_CID);    //ATT_CID = 4, For l2cap to use BLE
+	loc_addr.l2_bdaddr_type = BDADDR_LE_PUBLIC;
 
     bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
 
