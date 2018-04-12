80be6b9da4964d4f85abd3e798db8dc9baf009e0
Mandar Joshi <mandar1997@gmail.com>
2018-04-12 10:21:55 +0200

    Added l2cap architecture

---
+++ l2cap-client.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/l2cap.h>
+
+int main(int argc, char **argv)
+{
+    struct sockaddr_l2 addr = { 0 };
+    int s, status;
+    char *message = "hello!";
+    char dest[18] = "B8:27:EB:9B:D4:87";
+
+    if(argc < 2)
+    {
+        fprintf(stderr, "usage: %s <bt_addr>\n", argv[0]);
+        exit(2);
+    }
+
+    strncpy(dest, argv[1], 18);
+
+    // allocate a socket
+    s = socket(AF_BLUETOOTH, SOCK_SEQPACKET, BTPROTO_L2CAP);
+
+    // set the connection parameters (who to connect to)
+    addr.l2_family = AF_BLUETOOTH;
+    addr.l2_psm = htobs(0x1001);
+    str2ba( dest, &addr.l2_bdaddr );
+
+    // connect to server
+    status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
+
+    // send a message
+    if( status == 0 ) {
+        status = write(s, "hello!", 6);
+    }
+
+    if( status < 0 ) perror("uh oh");
+
+    close(s);
+}
