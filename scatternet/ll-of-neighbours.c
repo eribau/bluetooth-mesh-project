942b3a13fa38bb8d8e0a4b545233b93ac0812d98
abbenteprizer <abbe_g_93@hotmail.com>
2018-05-02 14:16:24 +0000

    first working datastructure

--- scatternet/ll-of-neighbours.c
+++ scatternet/ll-of-neighbours.c
@@ -2,54 +2,78 @@
 #include <string.h>
 #include "ll.h"
 
+#define MAX_ARR_LENGTH 16
+
 int i = 0;
 
-struct thing {
-  char bdaddr[18];
+// Nb is neighbour, nb_nb is nb's neighbour
+struct nb_object {
+ 	char nb_bdaddr[18];
+	char nb_nb_bdaddr[18];
 };
 
 // Adds LL to array element
-void add_nb(struct thing **ptr){
-
-  struct thing *thing = NULL;
-  thing = ll_new(thing);
-  strcpy(thing->bdaddr, "downiepie");
-  ptr[i] = thing;
+void add_nb(struct nb_object **ptr, char *nb_bdaddr, char *nb_nb_bdaddr){
+  struct nb_object *nb_object = NULL;
+  nb_object = ll_new(nb_object);
+  strcpy(nb_object->nb_bdaddr, nb_bdaddr);
+  strcpy(nb_object->nb_nb_bdaddr, nb_nb_bdaddr);
+  ptr[i] = nb_object;
   i++;
+}
+
+void add_nb_nb(struct nb_object **ptr, char *nb_bdaddr, char *nb_nb_bdaddr){
+	for(int j = 0; j < i; j++){
+		if(0 == strcmp(ptr[j]->nb_bdaddr, nb_bdaddr)){	
+			ll_foreach(ptr[j], it){
+				printf("this will print all nb_nb: %s\n",it->nb_nb_bdaddr);
+				if(0 == strcmp(it->nb_nb_bdaddr, nb_nb_bdaddr)){
+					printf("duplicate\n");
+					return;
+				}
+			}
+			ptr[j] = ll_new(ptr[j]);
+			strcpy(ptr[j]->nb_bdaddr, nb_bdaddr);
+			strcpy(ptr[j]->nb_nb_bdaddr, nb_nb_bdaddr);
+			printf("%s == %s\n", ptr[j]->nb_bdaddr, nb_bdaddr);
+			return;
+		} else {
+			continue;
+		}
+	}
+	printf("adding new nb_bdaddr\n");
+	add_nb(ptr, nb_bdaddr, nb_nb_bdaddr);
+
+}
+
+void print_nb(struct nb_object **ptr){
+	for(int j; j < i; j++){
+		printf("%s\n", ptr[j]->nb_bdaddr);
+	}
+}
+
+void print_nb_nb(struct nb_object **ptr, char *nb_bdaddr){
+	for(int j = 0; j < i; j++){
+		if(0 == strcmp(ptr[j]->nb_bdaddr, nb_bdaddr)){	
+			ll_foreach(ptr[j], it){
+				printf("%s: %s\n", nb_bdaddr, it->nb_nb_bdaddr);
+			}
+		}
+	}
 }
 
 int main()
 {
-
-  struct thing *ptr[16] = { 0 };
-
-  /* Related to LL */
-    struct thing *thing = NULL; /* list */
-
-    thing = ll_new(thing);
-    strcpy(thing->bdaddr, "world");
-    
-    thing = ll_new(thing);
-    strcpy(thing->bdaddr, "hejsanxdface");  
-    struct thing *thing3= NULL;
-    thing3 = ll_new(thing3);
-    strcpy(thing3->bdaddr, "downs");
-    add_nb(ptr);
-    add_nb(ptr);
-    
-    struct thing *thing2 = NULL; /* list */
-
-    ptr[0] = thing;
-    
-    ll_foreach(ptr[0], it){
-     printf("%s\n",it->bdaddr);
-    }
-    ll_foreach(ptr[1], it){
-     printf("%s\n",it->bdaddr);
-    }
-    ll_foreach(ptr[2], it){
-     printf("%s\n",it->bdaddr);
-    }
-    
+	printf("a\n");
+	struct nb_object *ptr[16] = { 0 };
+	add_nb_nb(ptr, "pi3", "pi1");
+	add_nb_nb(ptr, "pi3", "pi3");
+	add_nb_nb(ptr, "pi3", "pi4");
+	add_nb_nb(ptr, "pi3", "pi4");
+	add_nb_nb(ptr, "pi3", "pi5");
+	add_nb_nb(ptr, "pi4", "pi5");
+	
+	//print_nb(ptr);
+	print_nb_nb(ptr, "pi3");
    return 0;
 }
